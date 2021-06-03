5bb1f41f5bb777d235a7554ace8a2333317c1409
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2021-06-03 20:03:22 +0200

    import v1 bases files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 // JS
+
+// jQuery
+var moy = 0
+$('.slide_container_verticlal_1').on('input',function(e){
+    console.log("valeur n°1",$(this).val())
+});
+
+$('.slide_container_verticlal_2').on('input',function(e){
+    console.log("valeur n°2",$(this).val());
+});
+
+$('.slide_container_verticlal_3').on('input',function(e){
+    console.log("valeur n°3",$(this).val());
+});
+
+
+
+
+console.log("toutes mes données:",moy)
