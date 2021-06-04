dd6d77527f5c096a130d6ad8f9abca9c2a5941e7
Aouis20 <aouis.choukri@hetic.net>
2021-06-04 11:30:21 +0200

    html_css_jquery_v1_04_06

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 // JS
 
 // jQuery
-var moy = 0
+let v1 = [];
+let v2 = [];
+let v3 = [];
+var myv1 = 0;
+var myv2 = 0;
+var myv3 = 0;
+
 $('.slide_container_verticlal_1').on('input',function(e){
-    console.log("valeur n°1",$(this).val())
+    v1.push($(this).val());
+    var myv1 = v1[v1.length-1]
+    console.log(myv1)
+
 });
 
 $('.slide_container_verticlal_2').on('input',function(e){
-    console.log("valeur n°2",$(this).val());
+    v2.push($(this).val());
+    var myv2 = v2[v2.length-1]
+    console.log(myv2)
 });
 
 $('.slide_container_verticlal_3').on('input',function(e){
-    console.log("valeur n°3",$(this).val());
+    v3.push($(this).val());
+    var myv3 = v3[v3.length-1]
+    console.log(myv3)
 });
-
-
-
-
-console.log("toutes mes données:",moy)
