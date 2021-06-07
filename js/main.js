f6d6973fc2e7963ae3ab894c6fc7033fec6ec71c
Aouis20 <aouis.choukri@hetic.net>
2021-06-07 12:37:45 +0200

    countdown finalisation

--- js/main.js
+++ js/main.js
@@ -6,57 +6,42 @@ let v1 = 50;
 let v2 = 50;
 let v3 = 50;
 let moy = 0
+let cd_input = document.querySelector(".annee_fdm")
+let min_fdm = 2050
+
 
 
 // Curseurs n°1 (Viandes / Vege)
 
 $('.slide_container_verticlal_1').on('input',function(e){
     v1list.push($(this).val())
-    console.log("valeur n°1",v1list)
     v1 = v1list[v1list.length-1]
-    console.log(v1)
     moy = (Number(v1)+Number(v2)+Number(v3)) / 3
-    console.log(v1,v2,v3)
-    console.log("MOYENNE",Number(moy))
-});
+})
 
 
 // Curseurs n°2 (Population)
 
 $('.slide_container_verticlal_2').on('input',function(e){
     v2list.push($(this).val())
-    console.log("valeur n°2",v2list)
     v2 = v2list[v2list.length-1]
-    console.log(v2)
     moy = (Number(v1)+Number(v2)+Number(v3)) / 3
-    console.log(v1,v2,v3)
-    console.log("MOYENNE",Number(moy))
-});
+})
 
 
 // Curseurs n°3 (Energies)
 
 $('.slide_container_verticlal_3').on('input',function(e){
     v3list.push($(this).val())
-    console.log("valeur n°3",v3list)
     v3 = v3list[v3list.length-1]
-    console.log(v3)
     moy = (Number(v1)+Number(v2)+Number(v3)) / 3
-    console.log(v1,v2,v3)
-    console.log("MOYENNE",Number(moy))
-});
-
-
-
-// AVANT le final countdown déterminer les graphes
-
+})
 
 // FINAL COUNTDOWN
-let cd_input = document.querySelector(".annee_fdm")
+
 setInterval(function(){
-    
-    // Définition des unités de temps
-    let fdm = Math.floor(2050 + (2050*(moy/100)))
+    // Définition du temps restants
+    let fdm = Math.floor(min_fdm + min_fdm*(moy/100))
     
     // Affichage
     cd_input.innerHTML = fdm
