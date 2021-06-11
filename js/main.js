eefb1ccf80313da0462df67665e5e6d1aa212ce3
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2021-06-11 09:42:41 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-// JS
+// variables
+let v1list = [];
+let v2list = [];
+let v3list = [];
+let v1 = 50;
+let v2 = 50;
+let v3 = 50;
+let moy = 0
+let cd_input = document.querySelector(".texte_date_fin_du_monde")
+let min_fdm = 2050
+let today = new Date().getFullYear()
+let time_difference = min_fdm - today
+
+// Curseurs n°1 (Viandes / Vege)
+
+$('.slide_container_verticlal_1').on('input',function(e){
+    v1list.push($(this).val())
+    v1 = v1list[v1list.length-1]
+    moy = (Number(v1)+Number(v2)+Number(v3)) / 3
+})
+
+
+// Curseurs n°2 (Population)
+
+$('.slide_container_verticlal_2').on('input',function(e){
+    v2list.push(($(this).val())*25) // fois 25 pour retourner une valeur par rapport à 100
+    v2 = v2list[v2list.length-1]
+    moy = (Number(v1)+Number(v2)+Number(v3)) / 3
+})
+
+
+// Curseurs n°3 (Energies)
+
+$('.slide_container_verticlal_3').on('input',function(e){
+    v3list.push($(this).val())
+    v3 = v3list[v3list.length-1]
+    moy = (Number(v1)+Number(v2)+Number(v3)) / 3
+})
+
+// FINAL COUNTDOWN
+
+setInterval(function(){
+    // Définition du temps restants
+    console.log(moy)
+    let diff_moy = 1 - moy/100
+    let fdm = Math.floor(time_difference * diff_moy)
+    
+    // Affichage
+    cd_input.innerHTML = fdm + min_fdm
+    
+}, 1000);
